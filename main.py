b1a601488858b67c0b61e0e5c5ccc7a404712e27
Pawan Sunuwar <pawanman56@gmail.com>
2024-05-22 23:43:01 +0200

    add event listeners + response

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
-
 # Step 0: Load out token from somewhere safe
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
@@ -13,3 +12,43 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 intents: Intents = Intents.default()
 intents.message_content = True # NOQA
 client: Client = Client(intents=intents)
+
+# Step 2: Message functionality
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled properly)')
+        return
+    
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+    
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+# Step 3: Handling the startup for bot
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+# Step 4: Handling incoming messages
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+# Step 5: Main entry point
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
